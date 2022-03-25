330029bdf12f6ab51cefad7b9c9a460c741b4942
shuaibiqbal1 <47532556+shuaibiqbal1@users.noreply.github.com>
2022-03-25 16:00:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello india World');
+  res.send('Hello india  World');
 });
 
 app.listen(PORT, HOST);
